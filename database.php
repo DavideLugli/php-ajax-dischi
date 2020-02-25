bfaaaeba48cc7a18e19febac10c72ba5a000eb10
Davide Lugli <luglidavide.web@gmail.com>
2020-02-25 16:10:04 +0100

    milestone 1 ok

--- database.php
+++ database.php
@@ -18,5 +18,47 @@ $database = [
       'year' => 2019,
       'poster' => 'https://img.discogs.com/K1n8n3-YeXUNVuUl5GuJOubatUU=/fit-in/600x621/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-13123801-1550921427-8154.jpeg.jpg'
     ],
+    [
+      'title' => 'Machete Mixtape',
+      'author' => 'Machete Crew',
+      'year' => 2012,
+      'poster' => 'https://img.discogs.com/qUxNriVXNwfd6d1ZAqxdeOfWnXM=/fit-in/600x604/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-8441414-1461794823-5627.png.jpg'
+    ],
+    [
+      'title' => 'Meteora',
+      'author' => 'Linkin Park',
+      'year' => 2003,
+      'poster' => 'https://img.discogs.com/v71p1qmVPcG2LLDSyA0Vc-ZAzBo=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-567665-1245380635.jpeg.jpg'
+    ],
+    [
+      'title' => 'By The Way',
+      'author' => 'Red Hot Chili Peppers',
+      'year' => 2002,
+      'poster' => 'https://img.discogs.com/ec2fzmbKNmZ_HTInsIzX7XgABkY=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1162737-1197254678.jpeg.jpg'
+    ],
+    [
+      'title' => 'Bad Boy (Single)',
+      'author' => 'Landy',
+      'year' => 2019,
+      'poster' => 'https://s.mxmcdn.net/images-storage/albums5/8/8/8/5/3/4/46435888_350_350.jpg'
+    ],
+    [
+      'title' => 'Stoney',
+      'author' => 'Post Malone',
+      'year' => 2016,
+      'poster' => 'https://img.discogs.com/w_Ipzm0masl3d77w0GReers23vg=/fit-in/600x586/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-9506156-1510627924-8170.jpeg.jpg'
+    ],
+    [
+      'title' => 'Ossigeno - EP',
+      'author' => 'Rkomi',
+      'year' => 2018,
+      'poster' => 'https://img.discogs.com/iTtbcCugBEm5jC9qnwzdJtIL8gg=/fit-in/600x576/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-12208851-1530535543-9743.jpeg.jpg'
+    ],
+    [
+      'title' => '17',
+      'author' => 'XXXTentacion',
+      'year' => 2017,
+      'poster' => 'https://img.discogs.com/g9_M4XonGnh3KtzzpBQ2meUqOT4=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10750161-1529525185-4540.jpeg.jpg'
+    ]
 ]
  ?>
