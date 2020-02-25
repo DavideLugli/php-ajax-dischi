c3851d3f6cd0e14ca6663c550523f31a13958c49
Davide Lugli <luglidavide.web@gmail.com>
2020-02-25 15:52:34 +0100

    db working

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+$database = [
+    [
+      'title' => 'New Jersey',
+      'author' => 'Bon Jovi',
+      'year' => 1988,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+    ],
+    [
+      'title' => 'VII',
+      'author' => 'Koba La D',
+      'year' => 2018,
+      'poster' => 'https://img.discogs.com/WWaKlQ6yTtEopUIRsMIM9CcNWU8=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-12620335-1538751364-5403.jpeg.jpg'
+    ],
+    [
+      'title' => 'Amo',
+      'author' => 'Bring Me The Horizon',
+      'year' => 2019,
+      'poster' => 'https://img.discogs.com/K1n8n3-YeXUNVuUl5GuJOubatUU=/fit-in/600x621/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-13123801-1550921427-8154.jpeg.jpg'
+    ],
+]
+ ?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include __DIR__. '/database.php'; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>php ajax dischi</title>
+  <!-- mio style -->
+  <link rel="stylesheet" href="dist/style.css">
+</head>
+
+<body>
+  <header>
+    <div class="container">
+      <img src="img/logo.png" alt="logo" />
+    </div>
+  </header>
+
+  <div class="album-container container">
+    <?php foreach ($database as $cd) { ?>
+    <div class="album">
+      <img src="<?php echo $cd['poster']; ?>" alt="">
+      <h3><?php echo $cd['title']; ?></h3>
+      <span class="author"><?php echo $cd['author']; ?></span>
+      <span class="year"><?php echo $cd['year']; ?></span>
+    </div>
+    <?php } ?>
+  </div>
+
+
+  <script src="src/script.js" charset="utf-8"></script>
+</body>
+
+</html>
